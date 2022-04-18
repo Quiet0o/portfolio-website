c749ae1619069e7c0b85259b66f3d26a238a4588
Quiet0o <mikolajklosowski112@gmail.com>
2022-04-18 17:53:31 +0200

    clean up css code and adding RWD

--- src/HeroPage/HeroPage.jsx
+++ src/HeroPage/HeroPage.jsx
@@ -1,38 +1,48 @@
-import React from 'react'
+import React from "react";
 
-const HeroPage = ()=>{
-    return (
-        <>
-        <div className="hero-page">
-            <div className="welcome-page">
-                <span>Welcome my name is</span>
+const HeroPage = () => {
+  return (
+    <>
+      <div className="hero-page">
+        <div className="welcome-page">
+          <span>Welcome my name is</span>
 
-                <h1>Mikołaj Kłosowski</h1>
-                <h2><span>I'm web developer</span></h2>
-                <p>I'm web developer and UI/UX designer
-I'm specializes in creating website more accessible and faster .  My main goal is that website must be fast and accessible to people</p>
-            </div>
-
+          <h1>Mikołaj Kłosowski</h1>
+          <h2>
+            <span>I'm web developer</span>
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis in
+            placeat enim alias illo consectetur, deserunt est debitis fugit?
+            Ipsum dolorum non quis doloribus aspernatur optio id blanditiis,
+            maxime eos!
+          </p>
         </div>
-        <div class="arrow">
-                <span></span>
-                <span></span>
-                <span></span>
+      </div>
+      <div class="arrow">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className="about-page">
+        <div className="about-me-text">
+          <h2>
+            <span>0.1</span> I'm web developer <hr/>
+          </h2>
+          <p>
 
-</div>
-        <div className="about-page">
-                <div className="about-me-text">
-                <h2><span>I'm web developer</span></h2>
-                <p>I'm web developer and UI/UX designer
-I'm specializes in creating website more accessible and faster .  My main goal is that website must be fast and accessible to people</p>
-                </div>
-                <div className="about-me-img">
-                    <img src="https://agency.fenze.dev/images/mikolaj.png" alt="face" />
-                </div>
-
-            </div>
-        </>
-    )
-}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+            repellat vitae sit. Animi, odit praesentium libero amet quae alias
+            enim consequuntur officia sed atque ex soluta vitae maiores! Labore,
+            quidem?
+          </p>
+        </div>
+        <div className="about-me-img">
+          <img src="https://agency.fenze.dev/images/mikolaj.png" alt="face" />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default HeroPage;
