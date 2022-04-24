a0db3958bc340597fdbcb87e73562bf0a556e3db
Quiet0o <mikolajklosowski112@gmail.com>
2022-04-24 21:37:29 +0200

    fix useState issue

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 
 const Menu = () => {
   const [isActive, setActive] = useState(false);
